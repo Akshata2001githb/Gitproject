8d74e20f5997323cf14a05d68d5b757204c381a9
Akshata2001githb <aakshata191@gmail.com>
2025-01-06 21:38:14 +0530

    python file added

---
+++ operator.py
@@ -0,0 +1,93 @@
+#arithmatic operator
+print(3+99)#addition
+print(56.4+89.45+78.9)#addition
+print(+6)#it represents positive number
+print(+89.65)#it representes positive number
+# -
+print(89-65)#subtraction
+print(35.6-9.8-2.5)#subtraction
+print(-65.3)#it represents negative number
+print("=============================")
+# * asrik
+print(35.8*89)# multiplication
+#print(*34.2)#error
+print("=================")
+#**[exponentiation
+#syntax:base**power
+print(2**3)
+print(50**2)
+print(625**2)
+print(3.4**2)
+#division operators
+#1 floor division(//)
+# return "non-decimal quotient"
+print(10//2)#5
+print(31//3)#10
+#2 float division(/)
+# return "decimal quotient"
+print(10/2)#5.0
+print(31/3)#10.33333333333333333333333334
+#3 % modulus
+# return "reminder"
+print(10%2)#0
+print(31%3)#1
+
+####2.comparision/relational oprator:
+#>,<,>=,<=,==,!= ---->boolean val
+
+print((35+2)<(40-2))#true
+print((True**False) != ((10-10)*1))#true
+print(12>=14)#False
+print(53<=(50+3))#False
+print(52>=(64-12))#true
+print(True==((False+1)*True))#true
+print(99!=(11*(5+4)))#false
+# 3.logical operators
+# 3types
+# and ,or,not
+print((10>5)and(True >(True**False)))#false
+print(True and True and ((False*(False+True))+True))#true
+print((10+5) and (65>23) and True)#true
+print("==============================")
+print((True and False) or False or (0+1))# true
+print((False or True) or (True and False))#true
+print(not(((10-5) and (5-1)) or False))#true
+print("====================================")
+#4assignment operator
+a=10
+b=10
+c=a
+d=25
+c=d
+d=100
+d=35
+a=145
+print(a)
+print(b)
+print(c)
+#assignment operator(=)
+a=10
+b=20
+a+=b
+print(a)
+print(b)
+x=25
+y=2
+x**=y
+print(x)
+b=2
+c=0
+b+=c
+print(b)
+#identity operator(is,not is,comparision of id)
+#membership operator
+s1="abcd"
+print("a" in s1)
+print(a in s1)
+
+
+
+
+
+
+
